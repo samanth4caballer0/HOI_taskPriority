a5d1c7d2d7df54f3fcd11d7c467c830399f394f2
Raulmusito <raulmusito@gmail.com>
2025-04-08 15:33:22 +0200

    just for arm by now

--- src/TP_control_node.py
+++ src/TP_control_node.py
@@ -1,7 +1,7 @@
 import tf2_ros
 import rospy
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Pose, Point, Quaternion, TransformStamped
+from geometry_msgs.msg import PoseStamped, Point, Quaternion, TransformStamped
 from sensor_msgs.msg import JointState
 from math import sin, cos
 import tf
@@ -12,12 +12,12 @@ class TP_controller:
         rospy.init_node("TP_control_node")
         rospy.loginfo("Starting Task Priority Controller....")
         
-        self.publish_static_transform()  
+        #self.publish_static_transform()  
         self.MM = MobileManipulator() 
-        self.ee_pub = rospy.Publisher('/end_effector', Odometry, queue_size=10)
-        
+        #self.ee_pub = rospy.Publisher('/end_effector', Odometry, queue_size=10)
+        self.ee_pub = rospy.Publisher('/end_effector', PoseStamped, queue_size=10)
         # Timer for TP controller (Velocity Commands)
-        rospy.Subscriber('/turtlebot/joint_states', JointState, self.joint_pos_callback)
+        rospy.Subscriber('/swiftpro/joint_states', JointState, self.joint_pos_callback)
         rospy.Timer(rospy.Duration(0.1), self.visualize)
         
 
@@ -26,15 +26,26 @@ class TP_controller:
         # Calculate the end effector pose
         x, y, z, yaw = self.MM.getEndEffectorPose()
         
-        # create odom message
-        ee_pose = Odometry()
-        ee_pose.header.frame_id = 'footprint_new'
+        # create odom message Using Odometry message
+        """ ee_pose = Odometry()
+        ee_pose.header.frame_id = 'swiftpro/manipulator_base_link'
         ee_pose.child_frame_id = 'ee_frame'
         ee_pose.pose.pose.position = Point(x, y, z)
         
         # Convert yaw to quaternion
         quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw)
-        ee_pose.pose.pose.orientation = Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3])
+        ee_pose.pose.pose.orientation = Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]) """
+
+        # create odom message Using Pose message
+        ee_pose = PoseStamped()
+
+        ee_pose.header.frame_id = 'swiftpro/manipulator_base_link'
+        ee_pose.header.stamp = rospy.Time.now()        
+
+        ee_pose.pose.position = Point(x, y, z)
+        # Convert yaw to quaternion
+        quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw)
+        ee_pose.pose.orientation = Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3])
         
         #publish the odom message 
         self.ee_pub.publish(ee_pose)
@@ -47,7 +58,7 @@ class TP_controller:
         
         # Set header information
         static_transform.header.stamp = rospy.Time.now()
-        static_transform.header.frame_id = "turtlebot/kobuki/base_footprint"
+        static_transform.header.frame_id = "swiftpro/manipulator_base_link"
         static_transform.child_frame_id = "footprint_new"
         
         # Set translation (no translation in this case)
@@ -58,8 +69,8 @@ class TP_controller:
         # Set rotation (180° around X-axis)
         static_transform.transform.rotation.x = 0.0  # sin(π/2) = 1.0
         static_transform.transform.rotation.y = 0.0
-        static_transform.transform.rotation.z = -0.7071  # sin(π/4) = 0.7071
-        static_transform.transform.rotation.w = 0.7071  # sin(π/4) = 0.7071
+        static_transform.transform.rotation.z = 0  # sin(π/4) = 0.7071
+        static_transform.transform.rotation.w = 1  # sin(π/4) = 0.7071
         
         # Broadcast the transform
         static_broadcaster.sendTransform(static_transform)
@@ -68,9 +79,8 @@ class TP_controller:
         
         
     def joint_pos_callback(self, msg):
-        
         #filter out the passive joints 
-        if msg.name[0] == 'turtlebot/swiftpro/joint1':
+        if msg.name[0] == 'swiftpro/joint1':
 
         #extract the active joint position array 
             joint_pos = np.array([msg.position[0], msg.position[1], msg.position[2], msg.position[3]])
@@ -109,18 +119,21 @@ class MobileManipulator:
         q2 = self.q[1]
         q3 = self.q[2]
         q4 = self.q[3]
-        alpha = -np.pi/2.0      #rotation of the end effector frame with respect to the base frame
+        alpha = 0 # -np.pi/2.0      #rotation of the end effector frame with respect to the base frame
         
-        # Constants from the manipulator arm drawing 
+        # offset from joint 1 to the first articulation 
         self.bx = 0.0132
-        self.bz = 0.1080
+        self.bz = 0.108
+
+        #
+        self.bmz = 0 # 0.198  # 
+        self.bmx = 0 # 0.0507 # distance from the robot base frame to arm base frame
         
-        self.bmz = 0.198 
-        self.bmx = 0.0507 
-        
-        self.mx = 0.0565
+        # end effector offsets
+        self.mx = 0.0565  
         self.mz = 0.0722
-        
+
+        # distance of the links
         d1 = 0.1420
         d2 = 0.1588
         
@@ -131,9 +144,9 @@ class MobileManipulator:
         
         
         #forward kinematics to get ee position
-        x = l * cos(q1) + self.bmx
-        y = l * sin(q1) + self.bmx
-        z = (-self.bz - (d1 * cos(-q2)) - (d2 * sin(q3)) + self.mx - self.bmz) 
+        x = l * cos(q1)
+        y = l * sin(q1) 
+        z = (-self.bz - (d1 * cos(-q2)) - (d2 * sin(q3)) + self.mz - self.bmz) 
         yaw = q1 + q4 + alpha
         
         return x, y, z, yaw
