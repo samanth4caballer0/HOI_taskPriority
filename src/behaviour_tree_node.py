final code with fixed behavior tree + TP controller (-M changed sign of base angular velocity)

src/behaviour_tree_node.py
@@ -158,7 +158,7 @@ class ApproachBasetoObject(py_trees.behaviour.Behaviour):               ####TODO
         task_msg.desired = [self.blackboard.goal[0], self.blackboard.goal[1]]       #subtract from each (x and Y) a distance of like 15cm 
         self.task_publisher.publish(task_msg)
         print(self.err)
-        if  abs(self.err[0]) < 0.6 and abs(self.err[1]) < 0.6:
+        if  abs(self.err[0]) < 0.45 and abs(self.err[1]) < 0.45:
             self.logger.debug("  %s [ApproachBaseObject::Update() SUCCESS]" % self.name)
             return py_trees.common.Status.SUCCESS
         else: 
@@ -206,7 +206,7 @@ class ApproachEEtoObject(py_trees.behaviour.Behaviour):
         task_msg = TaskMsg()
         task_msg.ids = "1"
         task_msg.name = "ApproachEEtoObject"
-        task_msg.desired = [self.blackboard.goal[0], self.blackboard.goal[1], -0.25]     #-0.3 why? 
+        task_msg.desired = [self.blackboard.goal[0], self.blackboard.goal[1], -0.35]     #-0.3 why? 
         self.task_publisher.publish(task_msg)
         print(np.linalg.norm(self.err))
         if  np.linalg.norm(self.err)< 0.06:
@@ -257,10 +257,10 @@ class LowerEEToObject(py_trees.behaviour.Behaviour):
         task_msg = TaskMsg()
         task_msg.ids = "1"
         task_msg.name = "LowerEEtoObject"
-        task_msg.desired = [self.blackboard.goal[0], self.blackboard.goal[1], -0.153]
+        task_msg.desired = [self.blackboard.goal[0], self.blackboard.goal[1], -0.150]   #-0.153
         self.task_publisher.publish(task_msg)
         print(self.err)
-        if  np.linalg.norm(self.err)< 0.004:
+        if  self.err[2] < 0.004:
             self.logger.debug("  %s [LowerEEtoObject::Update() SUCCESS]" % self.name)
             return py_trees.common.Status.SUCCESS
         else:
@@ -329,7 +329,7 @@ class PickupObject(py_trees.behaviour.Behaviour):
         task_msg = TaskMsg()
         task_msg.ids = "1"
         task_msg.name = "PickupObject"
-        task_msg.desired = [self.blackboard.goal[0], self.blackboard.goal[1], -0.3]     #-0.3 why? 
+        task_msg.desired = [self.blackboard.goal[0], self.blackboard.goal[1], -0.33]     #-0.3 why? 
         self.task_publisher.publish(task_msg)
         time.sleep(1.0)
         # SUBSCRIBERS
@@ -347,7 +347,7 @@ class PickupObject(py_trees.behaviour.Behaviour):
         task_msg.desired = [self.blackboard.goal[0], self.blackboard.goal[1], -0.3]     #-0.3 why? 
         self.task_publisher.publish(task_msg)
         print(np.linalg.norm(self.err))
-        if  np.linalg.norm(self.err)< 0.003:
+        if  self.err[2] < 0.004:    #np.linalg.norm(self.err)< 0.003:    
             self.logger.debug("  %s [PickupObject::Update() SUCCESS]" % self.name)
             return py_trees.common.Status.SUCCESS
         else:

src/kinematics.py
@@ -88,7 +88,7 @@ class TP_controller:
         #command veloctiy publisher #TODO
         self.cmd_vel= rospy.Publisher('/turtlebot/kobuki/commands/velocity', Twist, queue_size=10)
         
-        odom_sim_topic = "/turtlebot/kobuki/SLAM/EKF_odom" # odometry topic for simulation
+        odom_sim_topic = "/turtlebot/kobuki/odom_ground_truth" # odometry topic for simulation
         #subscribe to the odometry topic to use later for transformation from world NED to robot base_footprint
         self.odom_sub = rospy.Subscriber(odom_sim_topic, Odometry, self.odomCallback) 
         self.rviz_sub = rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.rvizCallback) 
@@ -116,8 +116,8 @@ class TP_controller:
 
         s = []
         W = np.eye(6) 
-        W[0,0] = 70
-        W[1,1] = 70
+        W[0,0] = 100    #300?
+        W[1,1] = 100
         for task in self.tasks:
             task.update(self.MM) 
             if task.isActive():
@@ -157,7 +157,7 @@ class TP_controller:
         # publish the cmd_vel message
         cmd_vel = Twist()
         cmd_vel.linear.x = M2
-        cmd_vel.angular.z = M1*0.1
+        cmd_vel.angular.z = -M1
         self.cmd_vel.publish(cmd_vel)
         # print("cmd_vel", cmd_vel)
         # print ("dq: ", dq)
@@ -331,7 +331,7 @@ class TP_controller:
 
     ########## TASK MANAGER ##########
     def task_callback(self, msg):
-        print(len(self.tasks))
+        # print(len(self.tasks))
 
         if len(self.tasks) > 4 and self.tasks[-1].name != msg.name: # if the task is changed
             print("New task recieved")